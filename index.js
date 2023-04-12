b36894f85d04f39d258d27d1769a7b5e0ef97a34
Elisabeth Garcia <lisgarciae@gmail.com>
2023-04-12 15:24:07 -0400

    complete

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+const paragraph = document.querySelector("#text")
+document.addEventListener("DOMContentLoaded", function() {
+    paragraph.textContent = "This is really cool!"
+    console.log("The DOM has loaded")
+ 
+})
